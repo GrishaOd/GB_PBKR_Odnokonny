c75684dda8ab5810f4a7c3434549d7b1d50b50e0
Grisha <odnokonnygrisha@gmail.com>
2023-11-01 01:50:01 +0300

    feat: program file: fullarray function added

--- ProgramPart/Program.cs
+++ ProgramPart/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string[] FullArray (string[] array, int arlength)
+{
+for (int i = 0; i < arlength; i++)
+{
+    Console.Write($"Введите {i+1}ый элемент массива: ");
+    array[i] = Console.ReadLine();
+}
+return array;
+}
