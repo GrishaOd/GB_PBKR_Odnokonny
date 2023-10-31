3168fa9fd5d6bdc8f3559389a8a73d9b04e351fd
Grisha <odnokonnygrisha@gmail.com>
2023-11-01 01:57:34 +0300

    feat: gitignore file added, program file ended(everything works)

--- ProgramPart/Program.cs
+++ ProgramPart/Program.cs
@@ -18,21 +18,21 @@ for(int i = 0; i < array.Length; i++)
 return array;
 }
 
-string[] FilterArrayByLength(string[] array, int length)
+string[] FilterArrayByLength(string[] array, int filter_length)
 {
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= length)
+        if (array[i].Length <= filter_length)
         {
             count++;
         }
     }
     string[] newArray = new string[count];
     int j = 0;
-    for (int i = 0; i < array.length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= length)
+        if (array[i].Length <= filter_length)
         {
             newArray[j] = array[i];
             j++;
@@ -41,3 +41,15 @@ string[] FilterArrayByLength(string[] array, int length)
 
     return newArray;
 }
+
+Console.Write("Введите количество элементов массива: ");
+int len = int.Parse(Console.ReadLine());
+string[] first_array = new string[len];
+System.Console.WriteLine("Заполняем массив");
+FullArray(first_array, len);
+System.Console.WriteLine("Массив заполнен");
+System.Console.Write("Первоначальный массив: ");
+PrintArray(first_array);
+System.Console.WriteLine();
+System.Console.Write("Итоговый массив: ");
+PrintArray(FilterArrayByLength(first_array, 3));
